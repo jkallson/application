9f0aa503f9dbc312007a2aeb503bc5e2b83c328d
janar <janar.kallson@gmail.com>
2025-03-11 22:57:17 +0200

    feat: added more logging

--- backend/src/controllers/gameController.ts
+++ backend/src/controllers/gameController.ts
@@ -1,27 +1,29 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 
 import {GameService} from "../services/gameService";
 import {Game} from "../interfaces/Game";
 import {Reputation} from "../interfaces/Reputation";
 
 export const GameController = {
-    async startGame(req: Request, res: Response): Promise<void> {
+    async startGame(_req: Request, res: Response): Promise<void> {
         try {
             const game: Game = await GameService.startNewGame();
             res.status(200).json(game);
         } catch (error) {
-            res.status(500).json({ error: 'Failed to start the game.' });
+            console.log(`[ERROR] - startGame failed ${error}`)
+            res.status(500).json({error: 'Failed to start the game.'});
         }
     },
 
     async getReputation(req: Request, res: Response): Promise<void> {
-        const { gameId } = req.params;
+        const {gameId} = req.params;
 
         try {
             const reputation: Reputation = await GameService.getReputation(gameId);
             res.status(200).json(reputation);
         } catch (error) {
-            res.status(500).json({ error: 'Failed to fetch reputation.' });
+            console.log(`[ERROR] - getReputation failed ${error}`)
+            res.status(500).json({error: 'Failed to fetch reputation.'});
         }
     }
 }
--- backend/src/controllers/messageController.ts
+++ backend/src/controllers/messageController.ts
@@ -1,25 +1,27 @@
-import { Request, Response } from 'express';
-import { MessageService } from '../services/messageService';
+import {Request, Response} from 'express';
+import {MessageService} from '../services/messageService';
 import {Message, MessageSolveResponse} from "../interfaces/Message";
 
 export const MessageController = {
-    async getMessages(req: Request, res: Response) {
-        const { gameId } = req.params;
+    async getMessages(req: Request, res: Response): Promise<void> {
+        const {gameId} = req.params;
         try {
             const messages: Message[] = await MessageService.getMessages(gameId);
             res.status(200).json(messages);
         } catch (error) {
-            res.status(500).json({ error: 'Failed to retrieve messages.' });
+            console.log(`[ERROR] - getMessages failed ${error}`)
+            res.status(500).json({error: 'Failed to retrieve messages.'});
         }
     },
 
-    async solveMessage(req: Request, res: Response) {
-        const { gameId, messageId } = req.params;
+    async solveMessage(req: Request, res: Response): Promise<void> {
+        const {gameId, messageId} = req.params;
         try {
             const result: MessageSolveResponse = await MessageService.solveMessage(gameId, messageId);
             res.status(200).json(result);
         } catch (error) {
-            res.status(400).json({ error: `Failed to solve the message. ${error}` });
+            console.log(`[ERROR] - solveMessage failed ${error}`)
+            res.status(400).json({error: `Failed to solve the message. ${error}`});
         }
     }
 };
