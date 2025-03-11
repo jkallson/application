065c10cbc0864134526a2f19ce27af7345b454d7
janar <janar.kallson@gmail.com>
2025-03-12 00:05:28 +0200

    feat: less bloated vuetify

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="fill-height">
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-card flat>
+          Tere
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-card flat>
+          Tere
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+//
+</script>
