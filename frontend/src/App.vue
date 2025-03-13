a2bec8018a9666e2b7eeca808a0a343e6bf8a8dc
janar <janar.kallson@gmail.com>
2025-03-13 20:50:28 +0200

    feat: setup frontend again

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,11 @@
 <template>
-    <v-app>
-        <v-main>
-            <HelloWorld/>
-        </v-main>
-    </v-app>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<script setup lang="ts">
-import HelloWorld from "@/components/HelloWorld.vue";
-//
+<script lang="ts" setup>
+  //
 </script>
