d3472fc6f3518f61bd2f91bec6095d4166c6080e
janar <janar.kallson@gmail.com>
2025-03-16 12:08:44 +0200

    feat: add pinia

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -6,30 +6,37 @@
 
 // Composables
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import { routes } from 'vue-router/auto-routes'
+import DashboardView from '@/pages/dashboard/views/DashboardView.vue';
+import LandingPage from '@/pages/landingpage/views/LandingPage.vue';
+import Game2 from '@/pages/dashboard/views/Game2.vue';
+const routes = [
+    { path: '/', component: LandingPage },
+    { path: '/game', component: DashboardView },
+    { path: '/game2', component: Game2 },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
 })
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
 router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (!localStorage.getItem('vuetify:dynamic-reload')) {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
+    if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
+        if (!localStorage.getItem('vuetify:dynamic-reload')) {
+            console.log('Reloading page to fix dynamic import error')
+            localStorage.setItem('vuetify:dynamic-reload', 'true')
+            location.assign(to.fullPath)
+        } else {
+            console.error('Dynamic import error, reloading page did not fix it', err)
+        }
     } else {
-      console.error('Dynamic import error, reloading page did not fix it', err)
+        console.error(err)
     }
-  } else {
-    console.error(err)
-  }
 })
 
 router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
+    localStorage.removeItem('vuetify:dynamic-reload')
 })
 
 export default router
